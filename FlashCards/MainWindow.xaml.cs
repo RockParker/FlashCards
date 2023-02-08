1f8d3102f2e239db0ba0aa64de40f687d2edcccb
Ross Parker <rockparker9@gmail.com>
2023-02-08 11:33:33 -0500

    Add project files.

---
+++ FlashCards/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using FlashCards.Pages;
+using System.Windows;
+namespace FlashCards
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.MainFrame.Navigate(new IndexPage());
+        }
+    }
+}
---
+++ FlashCards/Pages/IndexPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FlashCards.Pages
+{
+    /// <summary>
+    /// Interaction logic for IndexPage.xaml
+    /// </summary>
+    public partial class IndexPage : Page
+    {
+        public IndexPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Show("Clicked");
+        }
+    }
+}
